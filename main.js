04a37d93e95f4dc26379fcdbe4ebfa9fe9094089
Luciano <lucco6823@gmail.com>
2021-07-29 17:04:31 +0200

    Terminato

--- main.js
+++ main.js
@@ -3,19 +3,45 @@ var numero;
 var numeri = [];
 
 // Richiamo la funzione della creazione di numeri casuali, ne faccio creare 5 (con valore da uno a 100) e evito le ripetizioni 
+
 while(numeri.length < 5){
     var numero = numbersRandom(1, 100);
     if(numeri.includes(numero) == false){
         numeri.push(numero);
     }
 }
+console.log(numeri)
 
 alert("Questi sono i 5 numeri che devi ricordare: " + numeri);
 
+// Da li parte un timer di 30 secondi.
+setTimeout(memorizzazione, 3000);
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+var i = 0;
+var numeriUtente;
+var numeriGiusti = [];
 
+function memorizzazione(){
+    for(i = 0; i < numeri.length; i++){
+        numeriUtente = parseInt(prompt("Inserisci i numeri che hai visto in precendenza: "));
+        if(isInArray(numeri, numeriUtente) == true){
+            numeriGiusti.push(numeriUtente);     
+        }
+        alert("Hai indovinato: " + numeriGiusti.length); 
+    }
+}
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 // FUNZIONI
 
 // Funziione che crea numeri casuali
 function numbersRandom(min, max){
     return risultato = Math.floor(Math.random() * max - min + 1) + min; 
+}
+
+//Funzione che permette di controllare se un elemento è presente nell'array
+function isInArray(array, element){
+    if(array.includes(element)){
+        return true;
+    }
+    return false;
 }
